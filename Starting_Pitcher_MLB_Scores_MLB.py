0e419bba43e297d92b8b18e6bae10d2c805ff550
EthanBauer <51236835+EthanBauer@users.noreply.github.com>
2022-04-19 17:16:38 -0400

    Update and rename SPitcher_Scores_MLB.py to Starting_Pitcher_MLB_Scores_MLB.py

--- Starting_Pitcher_MLB_Scores_MLB.py
+++ Starting_Pitcher_MLB_Scores_MLB.py
@@ -48,28 +48,3 @@ df['zTotal'].round(decimals=3)
 df.sort_values('zTotal', axis=0, ascending=False, inplace=True, na_position='first')
 df = df.reset_index(drop=True)
 df.to_csv(r"/Users/ethanbauer/Desktop/mlb_starting_pitcher_stats_zScore.csv", index = False, sep=',', encoding='utf-8')
-
-# # #function for zScore of given category
-# # def zScoreAdd(cat):
-# #     #create new list
-# #     zScore = []
-# #     #for every player in our data frame
-# #     for player in df.index:
-# #         #add to our list the zscore for the specific category ((category-category mean)/category standard deviation)
-# #         zScore.append((df.iloc[player][cat]-df[cat].mean())/df[cat].std())
-# #     #add a column to our data frame that is the zScore
-# #     df['z'+cat] = zScore
-# #     #round the decimals
-# #     df['z'+cat] = df['z'+cat].round(decimals=3)
-# # #for every category in our columns, call the zScoreAdd function
-# # for cat in categories:
-# #     zScoreAdd(cat)
-# # #Adding and rounding the final zScore and summing that new column
-# # df['zTotal'] = df.loc[0:338,['zR','zSB','zRBI','zBA','zHR']].sum(axis=1)
-# # df['zTotal'].round(decimals=3)
-
-# # #sort by the zTotal column
-# # df.sort_values('zTotal', axis=0, ascending=False, inplace=True, na_position='first')
-# # df = df.reset_index(drop=True)
-# # df.to_csv(r"/Users/ethanbauer/Desktop/mlb_stats.csv", index = False, sep=',', encoding='utf-8')
-
